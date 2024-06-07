1e2c9d4b05e1874b08e8fde3b19506adb028a50b
Sophie Donnet <sophie.donnet@inrae.fr>
2024-06-07 15:28:43 +0200

    ***

---
+++ Desert-final.py
@@ -0,0 +1,18 @@
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
+
+### Définir un tableau avec l'indice de la cellule en ligne, en colonne et son etat
+
+etats_possibles = ['V','D'] #  V = Vegetalisé, E = Empty 
+n = 50 
+paysage = np.zeros((n**2, 3))
+
+
+for i in range(n):
+    for j in range (n):
+          paysage(i;j)= random.choice(values) 
+
